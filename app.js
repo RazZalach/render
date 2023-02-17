5ef443948d47fb43c153cf3ae0e9c02818dbbafb
Raz <110323932+Idus-Raz@users.noreply.github.com>
2023-02-17 11:54:20 +0200

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5577;
-
 app.all("*",(req,res)=>{
     res.status(200).json({msg:"Hello From The Cloud"});
 });
